b23373b6dcb5c76c13a807d17bdf7f61edab212f
aidosmf <aidosmf@gmail.com>
2024-09-13 11:55:40 +0500

    feat: add tabs default ui

--- src/pages/validator.js
+++ src/pages/validator.js
@@ -3,6 +3,8 @@ import * as React from "react"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import Tabs from "react-bootstrap/Tabs"
+import Tab from "react-bootstrap/Tab"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -12,6 +14,8 @@ const content = {
   description:
     "Validates a given Lottie JSON file against the Lottie specification version.",
 }
+
+const tabCssClass = "border border-top-0 rounded-bottom p-3 shadow"
 
 const ValidatorPage = () => {
   return (
@@ -26,6 +30,26 @@ const ValidatorPage = () => {
           </Row>
         </Container>
       </section>
+      <Container className="py-5 mb-5">
+        <Row>
+          <Col>
+            <Tabs defaultActiveKey="url" id="justify-tab-example" justify>
+              <Tab eventKey="url" title="Url" className={tabCssClass}>
+                Tab content for url
+              </Tab>
+              <Tab eventKey="file" title="File" className={tabCssClass}>
+                Tab content for file
+              </Tab>
+              <Tab eventKey="text" title="Text" className={tabCssClass}>
+                Tab content for Text
+              </Tab>
+              <Tab eventKey="options" title="Options" className={tabCssClass}>
+                Tab content for Options
+              </Tab>
+            </Tabs>
+          </Col>
+        </Row>
+      </Container>
     </Layout>
   )
 }
